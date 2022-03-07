a484ae8401744726a413eef1d743c28c7068d32c
rodrigoEsquel <rodrigo.esquel@gmail.com>
2022-03-07 00:19:40 -0300

    Subida de la Tarea Clase 7 con fork del original
    Mirá mamá, hice un fork

---
+++ js/main.js
@@ -0,0 +1,89 @@
+function validarNombre(nombre) {
+    if (nombre.length === 0) {
+        return "Nombre debe tener al menos 1 caracter";
+    }
+    if (nombre.length > 50) {
+        return "Nombre debe tener menos de 50 caracteres";
+    }
+    if (!/^[a-z]+$/i.test(nombre)) {
+        return "Nombre debe contener solo letras";
+    }
+    return "";
+}
+function validarCiudad(ciudad) {
+    if (ciudad.length === 0) {
+        return "Ciudad no puede quedar vacio";
+    }
+    return "";
+}
+function validarDescripcionRegalo(descripcionRegalo) {
+    if (descripcionRegalo.length === 0) {
+        return "Descripcion del regalo debe tener al menos 1 caracter";
+    }
+    if (descripcionRegalo.length > 100) {
+        return "Descripcion del regalo debe tener menos de 100 caracteres";
+    }
+    if (!/^[a-z0-9]+$/i.test(descripcionRegalo)) {
+        return "Descripcion del regalo debe contener solo letras y numeros";
+    }
+    return "";
+}
+
+function validarFormulario(event) {
+    const $form = document.formulario;
+    const nombre = $form.nombre.value
+    const ciudad = $form.ciudad.value;
+    const comportamiento = $form.comportamiento.value
+    const descripcionRegalo = $form["descripcion-regalo"].value;
+
+    const errores = {
+        nombre: validarNombre(nombre),
+        ciudad: validarCiudad(ciudad),
+        "descripcion-regalo": validarDescripcionRegalo(descripcionRegalo)
+    }
+
+    manejarErrores(errores);
+    
+    event.preventDefault();
+}
+
+function manejarErrores(errores) {
+    let numeroErrores = 0;
+    document.querySelector("#errores").innerHTML = "";
+    Object.keys(errores).forEach(function(data) {
+        if (errores[data]) {         
+            manejarElementoError(document.formulario[data]);
+            agregarTextoPorID(errores[data],"errores");
+        } else {
+            manejarElementoOK(document.formulario[data]);
+        }
+        numeroErrores = numeroErrores + errores[data].length;
+    })
+    if (numeroErrores === 0) {
+        document.formulario.className = "oculto";
+        document.querySelector("#exito").className = "";
+        setTimeout(function(){
+            window.location.href = "./wishlist.html";
+        }, 5000);
+    }
+}
+
+function manejarElementoError(elemento) {
+    elemento.className = "error";
+    elemento.value = "";
+}
+
+function manejarElementoOK(elemento) {
+    elemento.className = "";
+}
+
+
+function agregarTextoPorID(texto,ID) {
+    const hijo = document.createElement("li");
+    hijo.innerText = texto;
+    const padre = document.querySelector(`#${ID}`);
+    padre.appendChild(hijo);
+}
+
+
+document.formulario.onsubmit = validarFormulario;
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,48 @@
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
+      validarNombre('') === 'Nombre debe tener al menos 1 caracter',
       'Validar nombre no validó que el nombre no sea vacío',
   );
 
   console.assert(
       validarNombre(
           '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
+      'Nombre debe tener menos de 50 caracteres',
       'Validar nombre no validó que el nombre sea menor a 50 caracteres',
   );
+  
+  console.assert(
+      validarNombre('1Fabricio') === 'Nombre debe contener solo letras',
+      'Validar nombre no validó que el nombre sea unicamente alfabético',
+  )
 }
 
+function probarValidarCiudad() {
+    console.assert(
+        validarCiudad("") === "Ciudad no puede quedar vacio",
+        "Validar ciudad no valido que el input no este vacio",
+    );
+}
+
+function probarValidarDescripcionRegalo() {
+    console.assert(
+        validarDescripcionRegalo("") === "Descripcion del regalo debe tener al menos 1 caracter",
+        "Validar descripcion regalo no valido que la desripcion contenga al menos un caracter",
+    )
+
+    console.assert(
+        validarDescripcionRegalo("123456789012345678901234567890132456789013245678901324567890123456789012345678901234567889012345678901") ===
+        "Descripcion del regalo debe tener menos de 100 caracteres",
+        "Validar descripcion regalo no valido que la descripcion sea menor a 100 caracteres",
+    );
+
+    console.assert(
+        validarDescripcionRegalo("Hola $123") === "Descripcion del regalo debe contener solo letras y numeros",
+        "Validar descripcion regalo no valido que la descripcion contenga unicamente letras y numeros",
+    )
+}
+
+
 probarValidarNombre();
+probarValidarCiudad();
+probarValidarDescripcionRegalo();
